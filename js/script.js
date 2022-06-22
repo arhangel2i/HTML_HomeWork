5bf769881f0c25f72b607d662132b937c93c721a
arhangel2i <arhangel2i@mail.ru>
2022-06-22 22:16:48 +0300

    update dz_03

--- js/script.js
+++ js/script.js
@@ -1,7 +1,4 @@
 //создаем переменную, в которую добавляем поиск элемента
-let userName = prompt("Введите ваше имя");
-let userField = document.getElementById("UserId");
-userField.innerText = "Пользователь: " + userName;
 
 let toggleButton = document.getElementById("toggle-theme-btn");
 let dark = false;
